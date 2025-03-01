c53716cf322852e6592fadb8ac830e4353a2d889
robotics-cbit <101251217+robotics-cbit@users.noreply.github.com>
2025-03-01 21:21:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 import os
 from dotenv import load_dotenv
 
-# Load environment variables from .env file
+# Load environment variables from .env file 
 load_dotenv()
 
 app = Flask(__name__, template_folder='templates')
